11b852afdc5e493cc1bc51047cf4058c85fefac6
david <dmu029018@gmail.com>
2020-10-19 22:29:25 +0200

    githubpagezs

---
+++ docs/precache-manifest.0ff588ae88ef9a6d26ceaac04373e864.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8658d42d34703bcf06d7f980eefa1c88",
+    "url": "/index.html"
+  },
+  {
+    "revision": "78f71be73f84ec5c7df1",
+    "url": "/static/css/main.9b402cdc.chunk.css"
+  },
+  {
+    "revision": "70ec3ba6e3debc9df543",
+    "url": "/static/js/2.a60d8d89.chunk.js"
+  },
+  {
+    "revision": "6627397752ec37793345b5d53931a151",
+    "url": "/static/js/2.a60d8d89.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "78f71be73f84ec5c7df1",
+    "url": "/static/js/main.e70661d4.chunk.js"
+  },
+  {
+    "revision": "c234e06047c3f13cae3d",
+    "url": "/static/js/runtime-main.451c3ec9.js"
+  }
+]);
